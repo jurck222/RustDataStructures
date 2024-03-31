def016bd8f462dabccb63ceb12a73d83c36ec186
jurck222 <56190152+jurck222@users.noreply.github.com>
2024-03-31 22:30:32 +0200

    :sparkles: Added hash table

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
-use crate::{binary_search_tree::BinarySearchTree, linked_list::LinkedList};
+use crate::{binary_search_tree::BinarySearchTree, hash_table::HashTable, linked_list::LinkedList};
 
 mod binary_search_tree;
+mod hash_table;
 mod linked_list;
 
 fn main() {
     run_linked_list();
     run_binary_search_tree();
+    run_hash_table();
 }
 
 fn run_linked_list() {
@@ -31,4 +33,13 @@ fn run_binary_search_tree() {
     println!("find 2 : {}", bst.find(2));
     println!("find 5 : {}", bst.find(5));
     println!("find 15 : {}", bst.find(15))
+}
+
+fn run_hash_table() {
+    let mut hash = HashTable::new();
+    hash.insert(String::from("Janez"), 1_000_000);
+    hash.insert(String::from("Marko"), 2_000_000);
+    hash.insert(String::from("Joza"), 1_500_000);
+    let x: String = String::from("Marko");
+    println!("value for {} = {}", x, hash.get(&x).unwrap());
 }
